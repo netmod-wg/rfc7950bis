620542eaa13f2740068c459d4449b0031d749628
kwatsen <kent@watsen.net>
2024-05-01 22:37:32 -0400

    now that draft is set, add a bunch of workflow/action files

---
+++ .github/pull_request_template.md
@@ -0,0 +1,9 @@
+## YANG-next Issue
+
+> Either: This PR closes netmod-wg/yang-next/#<issue-number>
+> Or: None
+
+
+## Additional Information
+
+> Any additional information that would not into the Issue Tracker or the draft itself?
---
+++ .github/workflows/gen_index_html.sh
@@ -0,0 +1,53 @@
+# enter directory
+cd mysite/
+
+# sort by last-update
+for d in */; do
+  DATE=`grep 'DATE:' $d/metadata.txt | sed 's/^DATE: //'`
+  dd=`echo $d | sed 's#/$##'`
+  echo "$dd $DATE" >> sort.txt
+done
+SORTED=`sort -k 1 sort.txt | awk '{print $1}'`
+
+# create the index.html file
+echo "<html>" > index.html
+echo "<head>" >> index.html
+echo "  <style>" >> index.html
+echo "    table { border-collapse: collapse; width: 100%; }" >> index.html
+echo "    tr:nth-child(odd){background-color: #f2f2f2}" >> index.html
+echo "    tr:nth-child(even){background-color: white}" >> index.html
+echo "    th    {background-color: #A0A0A0; color: white;}" >> index.html
+echo "    th,td {padding: 15px; text-align: left; vertical-align: top;}" >> index.html
+echo "  </style>" >> index.html
+echo "  <base target=\"_blank\">" >> index.html
+echo "</head>" >> index.html
+echo "<body>" >> index.html
+echo "<h1>Build Artifacts for rfc7950bis</h1>" >> index.html
+echo "<i>Most recent first</i>" >> index.html
+echo "<br><br>" >> index.html
+echo "<table>" >> index.html
+echo "  <tr>" >> index.html
+echo "    <th><b>Updated On</b></th>" >> index.html
+echo "    <th><b>Pull Request</b></th>" >> index.html
+echo "    <th><b>Branch Name</b></th>" >> index.html
+echo "    <th><b>Formats</b></th>" >> index.html
+echo "    <th><b>Actions</b></th>" >> index.html
+echo "  </tr>" >> index.html
+for d in $SORTED; do
+  echo "  <tr>" >> index.html
+  NUMBER=`grep NUMBER $d/metadata.txt | awk '{print $2}'`
+  TITLE=`grep TITLE $d/metadata.txt | awk '{print $2}'`
+  DATE=`grep DATE $d/metadata.txt | awk '{print $2}'`
+  echo "    <td>$DATE</td>" >> index.html
+  echo "    <td><a href=\"https://github.com/netmod-wg/rfc7950bis/pull/$NUMBER\">$TITLE (#$NUMBER)</a></td>" >> index.html
+  echo "    <td><a href=\"https://github.com/netmod-wg/rfc7950bis/tree/$d\">$d</a></td>" >> index.html
+  echo "    <td><a href=\"$d/draft-yn-netmod-rfc7950bis-00.html\">html</a> / <a href=\"$d/draft-yn-netmod-rfc7950bis-00.txt\">text</a> / <a href=\"$d/draft-yn-netmod-rfc7950bis-00.v2v3.xml\">xml</a></td>" >> index.html
+  if [ $d = "main" ]; then
+    echo "    <td><a href=\"https://author-tools.ietf.org/diff?doc_1=rfc7950&url_2=https://netmod-wg.github.io/rfc7950bis/main/draft-yn-netmod-rfc7950bis-00.txt\">Diff with RFC7950</a><br><a href=\"https://author-tools.ietf.org/api/iddiff?doc_1=draft-yn-netmod-rfc7950bis&url_2=https://netmod-wg.github.io/rfc7950bis/main/draft-yn-netmod-rfc7950bis-00.txt\">Diff with Datatracker</a><br>Upload to Datatracker (TBD)</td>" >> index.html
+  else
+    echo "    <td><a href=\"https://author-tools.ietf.org/api/iddiff?url_1=https://netmod-wg.github.io/rfc7950bis/main/draft-yn-netmod-rfc7950bis-00.txt&url_2=https://netmod-wg.github.io/rfc7950bis/$d/draft-yn-netmod-rfc7950bis-00.txt\">Diff with Main</a></td>" >> index.html
+  fi
+  echo "  </tr>" >> index.html
+done
+echo "</table>" >> index.html
+echo "</body>" >> index.html
